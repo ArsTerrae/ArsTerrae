29ccf527bea305f851bdddf341b4769e31c36c78
angelsl <hidingfromhidden@gmail.com>
2015-12-26 15:42:28 +0800

    Implement fruit tree shaking

--- src/main/scala/mc/harvestmoon/TreeShake.scala
+++ src/main/scala/mc/harvestmoon/TreeShake.scala
@@ -7,6 +7,8 @@ import net.minecraft.world.World
 import net.minecraftforge.event.entity.player.PlayerInteractEvent
 import org.apache.logging.log4j.{LogManager, Logger}
 
+import scala.collection.immutable.HashSet
+
 object TreeShake {
   private val log: Logger = LogManager.getLogger("HarvestMoon")
 
@@ -15,19 +17,34 @@ object TreeShake {
     if (e.world.isRemote) {
       return
     }
-    log.info(s"${e.action} @ ${e.x} ${e.y} ${e.z}")
     if (e.action == PlayerInteractEvent.Action.RIGHT_CLICK_BLOCK && e.getResult != Result.DENY) {
-      val block = e.world.getBlock(e.x, e.y, e.z)
-      if (block == TFCBlocks.fruitTreeWood) {
-        val topCoords = scanForTrunkTop(e.world, e.x, e.y, e.z)
-        log.info(s"${topCoords._1} ${topCoords._2} ${topCoords._3}")
+      if (e.world.getBlock(e.x, e.y, e.z) == TFCBlocks.fruitTreeWood) {
+        log.info(s"${e.action} @ ${e.x} ${e.y} ${e.z}")
+        (findFruits(e.world, (e.x, e.y, e.z))
+          foreach { case (x, y, z) =>
+          e.world.getBlock(x, y, z).onBlockActivated(e.world, x, y, z, e.entityPlayer, e.face, 0, 0, 0)
+        })
       }
     }
   }
 
-  def scanForTrunkTop(world: World, initialX: Int, initialY: Int, initialZ: Int): (Int, Int, Int) = {
-    (1 to 3).map(logCount => (initialX, initialY + logCount, initialZ)).takeWhile({ case (x, y, z) =>
-      world.getBlock(x, y, z) != TFCBlocks.fruitTreeLeaves && world.getBlock(x, y, z) != TFCBlocks.fruitTreeLeaves2 && !world.isAirBlock(x, y, z)
-    }).last
+  private def fruitRange(pos: (Int, Int, Int)) = {
+    val (x, y, z) = pos
+    ((for (xs <- x - 1 to x + 1; ys <- y - 1 to y + 1; zs <- z - 1 to z + 1) yield (xs, ys, zs))
+      filter { pos2 => pos != pos2 })
+  }
+
+  def findFruits(world: World, pos: (Int, Int, Int)): Set[(Int, Int, Int)] = {
+    val (x, y, z) = pos
+    ((HashSet((x + 1, y, z), (x - 1, y, z), (x, y, z + 1), (x, y, z - 1), (x, y + 1, z))
+      filter { case (x, y, z) => world.getBlock(x, y, z) == TFCBlocks.fruitTreeWood }
+      flatMap { pos => findFruits(world, pos) })
+      | (fruitRange(pos)
+      filter {
+      case (x, y, z) => {
+        val b = world.getBlock(x, y, z)
+        (b == TFCBlocks.fruitTreeLeaves || b == TFCBlocks.fruitTreeLeaves2) && ((world.getBlockMetadata(x, y, z) & 8) == 8)
+      }
+    }).toSet)
   }
 }
