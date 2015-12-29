7a750bf0d65aaf31e3fccc38871f48f75d10b212
Aaron Weiss <aaronweiss74@gmail.com>
2015-12-28 21:22:04 -0500

    Updated with some style changes.

--- src/main/scala/mc/harvestmoon/LogPile.scala
+++ src/main/scala/mc/harvestmoon/LogPile.scala
@@ -30,32 +30,32 @@ object LogPile {
   @SubscribeEvent
   def rightClick(e: PlayerInteractEvent): Unit = {
     if (e.world.isRemote
-      || e.action != PlayerInteractEvent.Action.RIGHT_CLICK_BLOCK
-      || e.getResult == Result.DENY
-      || e.world.getBlock(e.x, e.y, e.z) != TFCBlocks.logPile) {
+        || e.action != PlayerInteractEvent.Action.RIGHT_CLICK_BLOCK
+        || e.getResult == Result.DENY
+        || e.world.getBlock(e.x, e.y, e.z) != TFCBlocks.logPile) {
       return
     }
 
     val handStack = e.entityPlayer.getCurrentEquippedItem
     val teLogPile = e.world.getTileEntity(e.x, e.y, e.z).asInstanceOf[TELogPile]
-    if (handStack == null || handStack.getItem != TFCItems.logs
-      || teLogPile.getNumberOfLogs >= 16) {
+    if (handStack == null || handStack.getItem != TFCItems.logs || teLogPile.getNumberOfLogs >= 16) {
       return
     }
 
-    teLogPile.storage.zipWithIndex foreach {
-      case (item, index) =>
-        if (teLogPile.contentsMatch(index, handStack)) {
-          val add = math.min(4 - item.stackSize, handStack.stackSize)
-          teLogPile.injectContents(index, add)
-          handStack.stackSize -= add
-          e.setCanceled(true)
-        } else if (item == null) {
-          val add = math.min(handStack.stackSize, 4)
-          teLogPile.addContents(index, new ItemStack(handStack.getItem, add, handStack.getItemDamage))
-          handStack.stackSize -= add
-          e.setCanceled(true)
-        }
+    teLogPile.storage.zipWithIndex.foreach {
+      case (item, index) if (teLogPile.contentsMatch(index, handStack)) => {
+        val add = math.min(4 - item.stackSize, handStack.stackSize)
+        teLogPile.injectContents(index, add)
+        handStack.stackSize -= add
+        e.setCanceled(true)
+      }
+      case (null, index) => {
+        val add = math.min(handStack.stackSize, 4)
+        teLogPile.addContents(index, new ItemStack(handStack.getItem, add, handStack.getItemDamage))
+        handStack.stackSize -= add
+        e.setCanceled(true)
+      }
+      case _ => ()
     }
   }
 }
--- src/main/scala/mc/harvestmoon/TreeShake.scala
+++ src/main/scala/mc/harvestmoon/TreeShake.scala
@@ -36,19 +36,20 @@ object TreeShake {
   @SubscribeEvent
   def rightClick(e: PlayerInteractEvent): Unit =
     if (!e.world.isRemote
-      && e.action == PlayerInteractEvent.Action.RIGHT_CLICK_BLOCK
-      && e.getResult != Result.DENY
-      && e.world.getBlock(e.x, e.y, e.z) == TFCBlocks.fruitTreeWood
-      && e.world.getTileEntity(e.x, e.y, e.z).asInstanceOf[TEFruitTreeWood].isTrunk) {
-      (Random.shuffle(findFruits(e.world, (e.x, e.y, e.z)).toSeq).take(Random.nextInt(3) + 1)
-        foreach { case (x, y, z) =>
-        e.world.getBlock(x, y, z).onBlockActivated(e.world, x, y, z, e.entityPlayer, e.face, 0, 0, 0)
-        })
+        && e.action == PlayerInteractEvent.Action.RIGHT_CLICK_BLOCK
+        && e.getResult != Result.DENY
+        && e.world.getBlock(e.x, e.y, e.z) == TFCBlocks.fruitTreeWood
+        && e.world.getTileEntity(e.x, e.y, e.z).asInstanceOf[TEFruitTreeWood].isTrunk) {
+      Random.shuffle(findFruits(e.world, (e.x, e.y, e.z)).toSeq).take(Random.nextInt(3) + 1).foreach {
+        case (x, y, z) => e.world.getBlock(x, y, z)
+                           .onBlockActivated(e.world, x, y, z, e.entityPlayer, e.face, 0, 0, 0)
+      }
     }
 
   private def adjacentWood(world: World, pos: Point): Set[Point] = pos match {
-    case (x, y, z) => (HashSet((x + 1, y, z), (x - 1, y, z), (x, y, z + 1), (x, y, z - 1), (x, y + 1, z))
-      filter { case (x, y, z) => world.getBlock(x, y, z) == TFCBlocks.fruitTreeWood })
+    case (x, y, z) => HashSet((x + 1, y, z), (x - 1, y, z), (x, y, z + 1), (x, y, z - 1), (x, y + 1, z)).filter {
+      case (x, y, z) => world.getBlock(x, y, z) == TFCBlocks.fruitTreeWood
+    }
   }
 
   private def linkedWood(world: World, pos: Point, trv: Set[Point] = HashSet()): Set[Point] = pos match {
@@ -56,18 +57,21 @@ object TreeShake {
   }
 
   private def fruitRange(pos: Point) = pos match {
-    case (x, y, z) => for (xs <- x - 1 to x + 1; ys <- y - 1 to y + 1; zs <- z - 1 to z + 1; if (xs, ys, zs) != pos)
-      yield (xs, ys, zs)
+    case (x, y, z) => for {
+      xs <- x - 1 to x + 1
+      ys <- y - 1 to y + 1
+      zs <- z - 1 to z + 1
+      if (xs, ys, zs) != pos
+    } yield (xs, ys, zs)
   }
 
   private def fruitsSupportedByWood(world: World, pos: Point): Set[Point] =
-    (fruitRange(pos)
-      filter {
+    fruitRange(pos).filter {
       case (x, y, z) => {
         val b = world.getBlock(x, y, z)
         (b == TFCBlocks.fruitTreeLeaves || b == TFCBlocks.fruitTreeLeaves2) && ((world.getBlockMetadata(x, y, z) & 8) == 8)
       }
-    }).toSet
+    }.toSet
 
   private def findFruits(world: World, pos: Point): Set[Point] =
     linkedWood(world, pos).flatMap(p => fruitsSupportedByWood(world, p))
